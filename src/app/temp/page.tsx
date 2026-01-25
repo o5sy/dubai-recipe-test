cc7eac0d67d7d8521b4f61d116623593c265c3cb
o5sy <oosydev@gmail.com>
2026-01-25 23:08:15 +0900

    chore: 테스트 페이지 추가

---
+++ src/app/temp/page.tsx
@@ -0,0 +1,20 @@
+import CitizenCard from '@/components/result/CitizenCard';
+
+export default function TempPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4 py-8">
+      <CitizenCard
+        name="승욘"
+        regDate="2026.01.24"
+        siteUrl="www.dujjonku.fun"
+        type="은은한 장인형 두쫀쿠"
+        imageUrl="/img/istj.png"
+        traits={[
+          '성실하고 책임감이 강함',
+          '논리적이고 분석적인 사고방식',
+          '전통과 규칙을 중시함',
+        ]}
+      />
+    </div>
+  );
+}
