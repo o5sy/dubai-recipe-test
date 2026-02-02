ae23919673f63ff55e74749e97515c086c46b637
o5sy <oosydev@gmail.com>
2026-02-02 17:02:33 +0900

    refactor: 서드파티 스크립트 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import KakaoScript from '@/components/KakaoScript';
+import ThirdPartyScripts from '@/components/ThirdPartyScripts';
 import { Toaster } from '@/components/ui/toaster';
 import { SERVICE_URL } from '@/constants/app';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 
 const geistSans = Geist({
@@ -47,31 +45,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} antialiased`}>
-        <KakaoScript />
+        <ThirdPartyScripts />
         {children}
         <Toaster />
-        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
-        )}
-        {process.env.NEXT_PUBLIC_ADSENSE_ID && (
-          <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_ADSENSE_ID}`}
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
-        )}
-        {process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID && (
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-            `}
-          </Script>
-        )}
       </body>
     </html>
   );
